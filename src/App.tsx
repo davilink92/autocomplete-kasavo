b84aae38c36a537004b932be429a0f77a0f551d4
davilink92 <d.gambardella92@gmail.com>
2025-04-06 20:19:31 +0200

    feat: add title autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <div className="card">
+        <h2 style={{ textAlign: "left" }}>Search Neighbourhood</h2>
         <Autocomplete
           onSearch={fetchSuggestions}
           onSelect={(item) => console.log(item)}
